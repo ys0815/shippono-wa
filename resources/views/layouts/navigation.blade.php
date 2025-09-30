5e7ae8a9e21a37493dfaff2ecf6aa11f901812d2
yuuka-seki <asyst.seki@gmail.com>
2025-09-30 18:55:29 +0900

    header調整

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -10,7 +10,7 @@
                 <!-- Hamburger Menu Button (mobile only) -->
                 <div class="flex items-center mr-4 sm:hidden">
                     <button @click="sidebarOpen = ! sidebarOpen" aria-label="Open menu" 
-                        class="flex flex-col items-center justify-center p-2 rounded-md text-gray-500 hover:text-amber-700 hover:bg-amber-50 focus:outline-none focus:bg-amber-50 focus:text-amber-700 transition duration-150 ease-in-out">
+                        class="flex flex-col items-center justify-center p-2 rounded-md text-amber-900 hover:text-amber-700 hover:bg-amber-50 focus:outline-none focus:bg-amber-50 focus:text-amber-700 transition duration-150 ease-in-out">
                         <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
                         </svg>
